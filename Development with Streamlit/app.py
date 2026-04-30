c63b94b2322d7faca4f2b85d08f12aa51dff6f06
DevAvi404 <avipaul6712@gmail.com>
2026-04-30 11:29:45 +0600

    Update app.py with new content

--- Development with Streamlit/app.py
+++ Development with Streamlit/app.py
@@ -1,1 +1,17 @@
 # Streamlit App 
+from google import genai 
+import os 
+from dotenv import load_dotenv
+import streamlit as st 
+
+# create .env file and put your API key, otherwise it will not works!
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model = "gemini-3-flash-preview",
+    contents = "Give me an idea of Gemini API in 50 words"
+)
+
+st.markdown(response.text)
